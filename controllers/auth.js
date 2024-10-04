cb784d73d87df78263dfc77720be7382175cfbcc
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-10-05 03:26:25 +0530

    finished register func in backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,31 @@
 import { db } from "../db.js";
+import bcrypt from "bcryptjs";
 
-export const register = (req, res) => {};
+export const register = (req, res) => {
+  // CREATE USER
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(400).json("User Already Exsists");
+
+    // CREATE HASHED PASSWORD
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    // CREATE USER
+    const q = "INSERT INTO users (username, email, password, name) VALUE (?)";
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+
+      return res.status(200).json("User has Been Created !");
+    });
+  });
+};
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql2 from "mysql2";
 export const db = mysql2.createPool({
   host: "153.92.15.9",
   user: "u674260424_mystack",
-  password: "Kchordgroup*78979",
+  password: "Kchordgroup*789789",
   database: "u674260424_mystack",
   waitForConnections: true,
   connectionLimit: 10,
