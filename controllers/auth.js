34cc53e25df2c1adf7316c78a67fca86164187e7
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-10-05 03:43:20 +0530

    finished login func in backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   // CREATE USER
@@ -27,5 +28,43 @@ export const register = (req, res) => {
     });
   });
 };
-export const login = (req, res) => {};
-export const logout = (req, res) => {};
+
+export const login = (req, res) => {
+  // CHECK USER EXSIST
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User Not Found");
+
+    // IF USER EXSIST CHECK PASSWORD
+    const comparePassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!comparePassword)
+      return res.status(401).json("Invalid Username or Password");
+
+    // IF ALL OKAY , THEN CREATE THE JWT TOKENS
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
+    // CREATE THE USER DETAILS ARRAY FOR COOKIE
+    const { password, ...otherUserDetails } = data[0];
+
+    return res
+      .cookie("accesstoken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(otherUserDetails);
+  });
+};
+
+export const logout = (req, res) => {
+  return res
+    .clearCookie("accesstoken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User Logged Out");
+};
