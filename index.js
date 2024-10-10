297c04580dc35df1018b249c93ff74260815dae8
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-10-11 04:47:49 +0530

    project continive

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://192.168.8.103:5173",
+    origin: "http://192.168.8.100:5173",
   })
 );
 app.use(cookieParser());
