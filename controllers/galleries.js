c2be8f799a759d8e43894d6b9506355c0fe60263
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-10-13 07:05:36 +0530

    finished the gallery function on the server and tyied to implement a way to reduce the file size of the images[state:failed]

--- controllers/galleries.js
+++ controllers/galleries.js
@@ -2,7 +2,7 @@ import { db } from "../db.js";
 
 export const load = (req, res) => {
   const q =
-    "SELECT id,title,category,img,authorSlug,authorName FROM Vw_Blogs ORDER BY createdAt DESC";
+    "SELECT id,title,category,intro,img,authorSlug,authorName FROM Vw_Blogs ORDER BY createdAt DESC";
   db.query(q, (err, data) => {
     if (err) return res.status(500).json(err);
     if (data.length === 0) return res.status(404).json("No Data Found !");
