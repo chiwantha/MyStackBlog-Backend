9290fa8c8c608cdfea5e1bb3bdbf0b29d7b8a967
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2024-10-28 22:43:34 +0530

    free commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://192.168.8.100:5173",
+    origin: "http://192.168.8.101:5173",
   })
 );
 app.use(cookieParser());
